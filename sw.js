07d58eb524da09410a248d681adf2e7ab3b1a26e
Handreswes <andres23124@gmail.com>
2026-05-13 09:42:11 -0500

    Final Sync V68: sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-// SW v62 - KILL SWITCH
+// SW v63 - KILL SWITCH
 // This script unregisters itself and reloads to clear any SW loops
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((names) => {
-            return Promise.all(names.map(name => caches.delete(name)));
-        }).then(() => self.registration.unregister())
+            for (let name of names) caches.delete(name);
+        }).then(() => {
+            return self.registration.unregister();
+        }).then(() => {
+            return self.clients.matchAll();
+        }).then((clients) => {
+            clients.forEach(client => client.navigate(client.url));
+        })
     );
 });
