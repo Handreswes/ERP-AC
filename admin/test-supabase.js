da4c49bfa4ec1c8176a29eba062d8d7dfc0faec9
Handreswes <andres23124@gmail.com>
2026-06-23 16:24:26 -0500

    Final Sync V68: admin/test-supabase.js

--- admin/test-supabase.js
+++ admin/test-supabase.js
@@ -1,5 +1,5 @@
 const url = 'https://zuondbguopirimvfuehu.supabase.co/rest/v1/products?select=*';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1b25kYmd1b3BpcmltdmZ1ZWh1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjAzMjk2NiwiZXhwIjoyMDg3NjA4OTY2fQ.9Zja0di6OMtWwFyigiZiWnXo0burILHTVAuBOf6EhUE';
+const key = 'sb_publishable_29SdlPI3zzDkNvvEO38kOQ_2NTwiTC_';
 
 fetch(url, {
     headers: {
